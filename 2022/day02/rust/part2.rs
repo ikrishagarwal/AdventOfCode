cd3a9bfd22064d4fe4abf1c52be18eb1e7d4119e
Krish <68506145+KrishAgarwal2811@users.noreply.github.com>
2022-12-04 13:45:53 +0530

    day 02 in rust

---
+++ 2022/day02/rust/part2.rs
@@ -0,0 +1,44 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+fn main() {
+    let data = read_to_string("input.txt").unwrap();
+    let contents: Vec<&str> = data.split('\n').collect();
+
+    let mut score = 0;
+
+    let mut score_board = HashMap::new();
+
+    score_board.insert('A', ScoreBoard { x: 3, y: 4, z: 8 });
+
+    score_board.insert('B', ScoreBoard { x: 1, y: 5, z: 9 });
+
+    score_board.insert('C', ScoreBoard { x: 2, y: 6, z: 7 });
+
+    for line in contents {
+        let moves: Vec<char> = line //
+            .split(' ')
+            .map(|a| a.chars().nth(0).unwrap())
+            .collect();
+
+        score += score_board.get(&moves[0]).unwrap().get(&moves[1]);
+    }
+
+    println!("{}", score);
+}
+
+struct ScoreBoard {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl ScoreBoard {
+    fn get(&self, key: &char) -> i32 {
+        match key {
+            'X' => self.x,
+            'Y' => self.y,
+            'Z' => self.z,
+            _ => 0,
+        }
+    }
+}
